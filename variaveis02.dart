4ac9d12272df7c068fbca7ab038620edc11a7b8a
Marcos Alexandria <marcos.alexandria01@gmail.com>
2025-04-29 21:55:00 -0300

    Dart - Curso Completo: Valor Padrão (Variáveis) | Como declarar variaveis null

---
+++ variaveis02.dart
@@ -0,0 +1,14 @@
+void main() {
+  //para declarar uma variável, ela precisa ser inicializada
+
+  // agora a variável nome é do tipo String? (pode ser nula)
+  String? nome;
+  print(nome);
+
+  nome = "Marcos Santos de Alexandria";
+  print(nome);
+
+  nome = null; // agora a variável nome é nula
+  print(nome);
+
+}
