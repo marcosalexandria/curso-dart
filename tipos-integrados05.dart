88a0d6b40e6a59a954c9d468aec45c68666b9f4c
Marcos Alexandria <marcos.alexandria01@gmail.com>
2025-05-01 16:20:01 -0300

    Dart - Curso Completo: Sets (Tipos integrados)

---
+++ tipos-integrados05.dart
@@ -0,0 +1,11 @@
+void main(){
+  // sets
+  // em um conjunto set, não pode haver valores duplicados, o set não tem ordem, então não podemos acessar os valores por índice
+
+  Set<String> nomes = {'Lucas', 'João', 'Maria', 'Ana', 'Lucas'}; // o valor 'Lucas' só aparece uma vez no conjunto
+  
+  Set<String> nomes2 = Set(); // criando um set vazio
+  Set<String> nomes3 = {}; // criando um set vazio
+  
+  print(nomes); 
+}
