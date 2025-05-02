aa5a0313f0c30fe6bbfa459b37ef594fa02cff5a
Marcos Alexandria <marcos.alexandria01@gmail.com>
2025-05-02 17:47:30 -0300

    Dart - Curso Completo: Runes (Tipos integrados)

---
+++ tipos-integrados07.dart
@@ -0,0 +1,7 @@
+void main(){
+  // runes são uma representação de caracteres unicode
+  // esses caracteres são emojis, símbolos e letras de outros idiomas
+  print('\u2665');
+  print('\u{1F600}'); // As chaves são necessárias para caracteres com mais de 4 dígitos
+  print('\u{1F913}'); 
+}
